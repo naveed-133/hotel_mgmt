6df3bf5338cddac60faa6377b2fcdeae429c8795
naveed-133 <xssnaveed@gmail.com>
2024-02-27 11:24:54 +0500

    import usenavigate

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,6 +10,7 @@ import { useState } from "react";
 import CircularProgress from '@mui/material/CircularProgress';
 import PasswordField from "../components/PasswordField";
 import { Container } from '@mui/material';
+import { useNavigate } from "react-router-dom";
 
  const HEADER_DATA = [
     {
